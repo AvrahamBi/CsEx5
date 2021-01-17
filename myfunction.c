20b49a4923a31a458f019f7af06cb719ab88ba0b
AvrahamBi <avrahambarilan@gmail.com>
2021-01-17 02:23:30 +0200

    need to write documnetation

--- myfunction.c
+++ myfunction.c
@@ -12,28 +12,25 @@ typedef struct {
     int blue;
 } pixel_sum;
 
-
 /*
-* Apply the kernel over each pixel.
-* Ignore pixels where the kernel exceeds bounds. These are pixels with row index smaller than kernelSize/2 and/or
-* column index smaller than kernelSize/2
+* this function smooth an image
 */
-void smooth(int dim, pixel* src, pixel* dst, register int kernelScale, register int filter, register int sharp) {
+void smooth(int M, pixel* src, pixel* dst, register int kernelScale, register int isFilter, register int isSharp) {
 
-    register int i, j, size = dim - 1, midRow = dim, aboveRow, belowRow, min_intensity, max_intensity, min_row, min_col, max_row, max_col, midRowJ,
-    aboveRowJ, belowRowJ, temp, ii, jj, midRow1, pColor, temp1, temp2, iiLimit, jjLimit;
+    register int i, j, size = M - 1, mRow = M, upperRow, underRow, min_intensity, max_intensity, min_row, min_col, max_row, max_col, mRowJ,
+    upperRowJ, underRowJ, temp, ii, jj, tempRow, tempColor, temp1, temp2, maxIi, maxJj;
     pixel current_pixel, loop_pixel;
     for (i = 1; i < size; ++i) {
-        aboveRow = midRow + dim;
-        belowRow = midRow - dim;
+        upperRow = mRow + M;
+        underRow = mRow - M;
         for (j = 1; j < size; ++j) {
             //initializing pixel_sum locally
             pixel_sum sum = { 0, 0, 0 };
             min_intensity = 766; // arbitrary value that is higher than maximum possible intensity, which is 255*3=765
             max_intensity = -1; // arbitrary value that is lower than minimum possible intensity, which is 0
-            midRowJ = midRow + j;
-            aboveRowJ = aboveRow + j;
-            belowRowJ = belowRow + j;
+            mRowJ = mRow + j;
+            upperRowJ = upperRow + j;
+            underRowJ = underRow + j;
             //
             /* sharp Kernel:
             * [-1, -1, -1]
@@ -41,87 +38,84 @@ void smooth(int dim, pixel* src, pixel* dst, register int kernelScale, register 
             * [-1, -1, -1]
             * adding the values manually to avoid extra loops and funciton calls.
             */
-            if (sharp) {
-                sum.red = (((int)src[midRowJ].red) << 3 ) + (int)src[midRowJ].red;
-                sum.green = (((int)src[midRowJ].green) << 3 ) + (int)src[midRowJ].green;
-                sum.blue = (((int)src[midRowJ].blue) << 3 ) + (int)src[midRowJ].blue;
-                // todo check which is faster
-                /*sum.green = (int)((src[midRowJ].green << 3 ) + src[midRowJ].green);
-                sum.blue = (int)((src[midRowJ].blue << 3 ) + src[midRowJ].blue);
-                sum.red = (int)((src[midRowJ].red << 3 ) + src[midRowJ].red);*/
+            if (isSharp) {
 
-                temp = midRowJ + 1;
-                sum.red -= ((int)src[temp].red);
-                sum.green -= ((int)src[temp].green);
-                sum.blue -= ((int)src[temp].blue);
-                temp -= 2;
-                sum.red -= ((int)src[temp].red);
-                sum.green -= ((int)src[temp].green);
-                sum.blue -= ((int)src[temp].blue);
+                sum.green = (src[mRowJ].green << 3)  + src[mRowJ].green;
+                sum.blue = (src[mRowJ].blue << 3)  + src[mRowJ].blue;
+                sum.red = (src[mRowJ].red << 3)  + src[mRowJ].red;
 
-                sum.red -= ((int)src[aboveRowJ].red);
-                sum.green -= ((int)src[aboveRowJ].green);
-                sum.blue -= ((int)src[aboveRowJ].blue);
-                temp = aboveRowJ + 1;
-                sum.red -= ((int)src[temp].red);
-                sum.green -= ((int)src[temp].green);
-                sum.blue -= ((int)src[temp].blue);
+                temp = mRowJ + 1;
+                sum.red -= src[temp].red;
+                sum.green -= src[temp].green;
+                sum.blue -= src[temp].blue;
                 temp -= 2;
-                sum.red -= ((int)src[temp].red);
-                sum.green -= ((int)src[temp].green);
-                sum.blue -= ((int)src[temp].blue);
+                sum.red -= src[temp].red;
+                sum.green -= src[temp].green;
+                sum.blue -= src[temp].blue;
 
-                sum.red -= ((int)src[belowRowJ].red);
-                sum.green -= ((int)src[belowRowJ].green);
-                sum.blue -= ((int)src[belowRowJ].blue);
-                temp = belowRowJ + 1;
-                sum.red -= ((int)src[temp].red);
-                sum.green -= ((int)src[temp].green);
-                sum.blue -= ((int)src[temp].blue);
+                sum.red -= src[upperRowJ].red;
+                sum.green -= src[upperRowJ].green;
+                sum.blue -= src[upperRowJ].blue;
+                temp = upperRowJ + 1;
+                sum.red -= src[temp].red;
+                sum.green -= src[temp].green;
+                sum.blue -= src[temp].blue;
                 temp -= 2;
-                sum.red -= ((int)src[temp].red);
-                sum.green -= ((int)src[temp].green);
-                sum.blue -= ((int)src[temp].blue);
+                sum.red -= src[temp].red;
+                sum.green -= src[temp].green;
+                sum.blue -= src[temp].blue;
+
+                sum.red -= src[underRowJ].red;
+                sum.green -= src[underRowJ].green;
+                sum.blue -= src[underRowJ].blue;
+                temp = underRowJ + 1;
+                sum.red -= src[temp].red;
+                sum.green -= src[temp].green;
+                sum.blue -= src[temp].blue;
+                temp -= 2;
+                sum.red -= src[temp].red;
+                sum.green -= src[temp].green;
+                sum.blue -= src[temp].blue;
             }
                 // Blur Kernel is 3x3 of 1's
             else {
-                sum.red += ((int)src[midRowJ].red);
-                sum.green += ((int)src[midRowJ].green);
-                sum.blue += ((int)src[midRowJ].blue);
-                temp = midRowJ + 1;
-                sum.red += ((int)src[temp].red);
-                sum.green += ((int)src[temp].green);
-                sum.blue += ((int)src[temp].blue);
+                sum.red += src[mRowJ].red;
+                sum.green += src[mRowJ].green;
+                sum.blue += src[mRowJ].blue;
+                temp = mRowJ + 1;
+                sum.red += src[temp].red;
+                sum.green += src[temp].green;
+                sum.blue += src[temp].blue;
                 temp -= 2;
-                sum.red += ((int)src[temp].red);
-                sum.green += ((int)src[temp].green);
-                sum.blue += ((int)src[temp].blue);
+                sum.red += src[temp].red;
+                sum.green += src[temp].green;
+                sum.blue += src[temp].blue;
 
-                sum.red += ((int)src[aboveRowJ].red);
-                sum.green += ((int)src[aboveRowJ].green);
-                sum.blue += ((int)src[aboveRowJ].blue);
-                temp = aboveRowJ + 1;
-                sum.red += ((int)src[temp].red);
-                sum.green += ((int)src[temp].green);
-                sum.blue += ((int)src[temp].blue);
+                sum.red += src[upperRowJ].red;
+                sum.green += src[upperRowJ].green;
+                sum.blue += src[upperRowJ].blue;
+                temp = upperRowJ + 1;
+                sum.red += src[temp].red;
+                sum.green += src[temp].green;
+                sum.blue += src[temp].blue;
                 temp -= 2;
-                sum.red += ((int)src[temp].red);
-                sum.green += ((int)src[temp].green);
-                sum.blue += ((int)src[temp].blue);
+                sum.red += src[temp].red;
+                sum.green += src[temp].green;
+                sum.blue += src[temp].blue;
 
-                sum.red += ((int)src[belowRowJ].red);
-                sum.green += ((int)src[belowRowJ].green);
-                sum.blue += ((int)src[belowRowJ].blue);
-                temp = belowRowJ + 1;
-                sum.red += ((int)src[temp].red);
-                sum.green += ((int)src[temp].green);
-                sum.blue += ((int)src[temp].blue);
+                sum.red += src[underRowJ].red;
+                sum.green += src[underRowJ].green;
+                sum.blue += src[underRowJ].blue;
+                temp = underRowJ + 1;
+                sum.red += src[temp].red;
+                sum.green += src[temp].green;
+                sum.blue += src[temp].blue;
                 temp -= 2;
-                sum.red += ((int)src[temp].red);
-                sum.green += ((int)src[temp].green);
-                sum.blue += ((int)src[temp].blue);
+                sum.red += src[temp].red;
+                sum.green += src[temp].green;
+                sum.blue += src[temp].blue;
             }
-            if (filter) {
+            if (isFilter) {
                 //checkign min/max locally to avoid funciton calls + avoid using it every iteration of the loop
 
                 if (i - 1 > 0) {
@@ -130,47 +124,45 @@ void smooth(int dim, pixel* src, pixel* dst, register int kernelScale, register 
                 if (j - 1 >= 0) {
                     temp2 = j - 1;
                 }
-                iiLimit = i + 1;
-                jjLimit = j + 1;
-                if (dim - 1 < iiLimit) {
-                    iiLimit = dim - 1;
+                maxIi = i + 1;
+                maxJj = j + 1;
+                if (M - 1 < maxIi) {
+                    maxIi = M - 1;
                 }
-                if (dim - 1 < jjLimit) {
-                    jjLimit = dim - 1;
+                if (M - 1 < maxJj) {
+                    maxJj = M - 1;
                 }
 
-                // todo its four 'for' loops its most be more efficient
+                tempRow = temp1 * M;
+                for (ii = temp1; ii <= maxIi; ++ii) {
 
-                midRow1 = temp1 * dim;
-                for (ii = temp1; ii <= iiLimit; ++ii) {
+                    for (jj = temp2; jj <= maxJj; ++jj) {
 
-                    for (jj = temp2; jj <= jjLimit; ++jj) {
+                        loop_pixel = src[tempRow + jj];
 
-                        loop_pixel = src[midRow1 + jj];
+                        //calculating  only once instead of 3 times
 
-                        //calculating pColor only once instead of 3 times
+                        tempColor = loop_pixel.red + loop_pixel.green + loop_pixel.blue;
 
-                        pColor = (int)(loop_pixel.red + loop_pixel.green + loop_pixel.blue);
-
-                        if (pColor <= min_intensity) {
-                            min_intensity = pColor;
+                        if (tempColor <= min_intensity) {
+                            min_intensity = tempColor;
                             min_row = ii;
                             min_col = jj;
                         }
-                        if (pColor > max_intensity) {
-                            max_intensity = pColor;
+                        if (tempColor > max_intensity) {
+                            max_intensity = tempColor;
                             max_row = ii;
                             max_col = jj;
                         }
                     }
-                    midRow1 += dim;
+                    tempRow += M;
                 }
 
-                pixel p1 = src[min_row * dim + min_col];
-                pixel p2 = src[max_row * dim + max_col];
-                sum.red -= (int)(p1.red + p2.red);
-                sum.green -= (int)(p1.green + p2.green);
-                sum.blue -= (int)(p1.blue + p2.blue);
+                pixel p1 = src[min_row * M + min_col];
+                pixel p2 = src[max_row * M + max_col];
+                sum.red -= (p1.red + p2.red);
+                sum.green -= (p1.green + p2.green);
+                sum.blue -= (p1.blue + p2.blue);
             }
             sum.red /= kernelScale;
             sum.green /= kernelScale;
@@ -199,52 +191,52 @@ void smooth(int dim, pixel* src, pixel* dst, register int kernelScale, register 
             }
             current_pixel.blue = (unsigned char)sum.blue;
 
-            dst[midRow + j] = current_pixel;
+            dst[mRow + j] = current_pixel;
         }
-        midRow += dim;
+        mRow += M;
     }
 }
 
-void doConvolution(register int kernelScale, register int filter, register int sharp) {
+void doConvolution(register int kernelScale, register int isFilter, register int isSharp) {
     //calculating size only once, reduce funciton calls (sizeof) as well
-    register int size = m * n * sizeof(pixel), row, col, index1, index2, thisRow = 0;
-    pixel* pixelsImg = malloc(size);
-    pixel* backupOrg = malloc(size);
+    register int M = m, N = n, size = M * N * sizeof(pixel), row, col, rowCol, threeRows, temp3 = 0;
+    pixel* src = malloc(size);
+    pixel* dst = malloc(size);
     //implementing called functions.
-    for (row = 0; row < m; ++row) {
+    for (row = 0; row < M; ++row) {
 
-        index1 = thisRow;
+        rowCol = temp3;
 
-        for (col = 0; col < n; ++col) {
-            index2 = index1 + index1 + index1;
-            pixelsImg[index1].red = image->data[index2];
-            pixelsImg[index1].green = image->data[index2 + 1];
-            pixelsImg[index1].blue = image->data[index2 + 2];
+        for (col = 0; col < N; ++col) {
+            threeRows = rowCol + rowCol + rowCol;
+            src[rowCol].red = image->data[threeRows];
+            src[rowCol].green = image->data[threeRows + 1];
+            src[rowCol].blue = image->data[threeRows + 2];
 
-            backupOrg[index1].red = pixelsImg[index1].red;
-            backupOrg[index1].green = pixelsImg[index1].green;
-            backupOrg[index1].blue = pixelsImg[index1].blue;
-            ++index1;
+            dst[rowCol].red = src[rowCol].red;
+            dst[rowCol].green = src[rowCol].green;
+            dst[rowCol].blue = src[rowCol].blue;
+            ++rowCol;
         }
-        thisRow += n;
+        temp3 += N;
     }
 
-    smooth(m, backupOrg, pixelsImg, kernelScale, filter, sharp);
+    smooth(M, dst, src, kernelScale, isFilter, isSharp);
 
-    thisRow = 0;
-    for (row = 0; row < m; ++row) {
-        index1 = thisRow;
-        for (col = 0; col < n; ++col) {
-            index2 = index1 + index1 + index1;
-            image->data[index2] = pixelsImg[index1].red;
-            image->data[index2 + 1] = pixelsImg[index1].green;
-            image->data[index2 + 2] = pixelsImg[index1].blue;
-            ++index1;
+    temp3 = 0;
+    for (row = 0; row < M; ++row) {
+        rowCol = temp3;
+        for (col = 0; col < N; ++col) {
+            threeRows = rowCol + rowCol + rowCol;
+            image->data[threeRows] = src[rowCol].red;
+            image->data[threeRows + 1] = src[rowCol].green;
+            image->data[threeRows + 2] = src[rowCol].blue;
+            ++rowCol;
         }
-        thisRow += n;
+        temp3 += N;
     }
-    free(pixelsImg);
-    free(backupOrg);
+    free(src);
+    free(dst);
 }
 
 void myfunction(Image* image, char* srcImgpName, char* blurRsltImgName, char* sharpRsltImgName, char* filteredBlurRsltImgName, char* filteredSharpRsltImgName, char flag) {
@@ -252,6 +244,7 @@ void myfunction(Image* image, char* srcImgpName, char* blurRsltImgName, char* sh
     if (flag == '1') {
         // blur image
         doConvolution(9, 0, 0);
+
 
         // write result image to file
         writeBMP(image, srcImgpName, blurRsltImgName);
